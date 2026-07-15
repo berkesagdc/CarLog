95486512413e6147e47099f91faea92d0ee72afc
Berke Sağdıç <berkealisagdic@gmail.com>
2026-07-15 22:46:39 +0300

    Add FuelLog model

--- Car.swift
+++ Car.swift
@@ -23,13 +23,16 @@ class Car {
         self.plate = plate
         self.mileage = mileage
     }
-
+    
     var brand: String
     var model: String
     var year: String
     var plate: String
     var mileage: String
-
+    
     @Relationship(deleteRule: .cascade)
     var maintenances: [Maintenance] = []
+    
+    @Relationship(deleteRule: .cascade, inverse: \FuelLog.car)
+    var fuelLogs: [FuelLog] = []
 }
---
+++ FuelLog.swift
@@ -0,0 +1,25 @@
+import Foundation
+import SwiftData
+
+@Model
+class FuelLog {
+
+    var date: Date
+    var mileage: String
+    var liters: String
+    var totalPrice: String
+
+    var car: Car?
+
+    init(
+        date: Date = Date(),
+        mileage: String,
+        liters: String,
+        totalPrice: String
+    ) {
+        self.date = date
+        self.mileage = mileage
+        self.liters = liters
+        self.totalPrice = totalPrice
+    }
+}
